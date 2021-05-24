7de143540175797ebb6d46e251dc0496ea870b36
Tomas <tomasmrom@gmail.com>
2021-05-24 13:01:28 -0300

    Agrego el proyecto "dice rolling sim"

---
+++ dice_rolling_simulator/dice.py
@@ -0,0 +1,25 @@
+import random
+import os
+
+os.system('cls')
+print('\nBienvenidos al "dice rolling simulator".\n\n')
+
+dado = {
+1 : "+-------+\n|       |\n|   0   |\n|       |\n+-------+\n",
+2 : "+-------+\n|     0 |\n|       |\n| 0     |\n+-------+\n",
+3 : "+-------+\n|     0 |\n|   0   |\n| 0     |\n+-------+\n",
+4 : "+-------+\n| 0   0 |\n|       |\n| 0   0 |\n+-------+\n",
+5 : "+-------+\n| 0   0 |\n|   0   |\n| 0   0 |\n+-------+\n",
+6 : "+-------+\n| 0   0 |\n| 0   0 |\n| 0   0 |\n+-------+\n"
+}
+
+continuar = 'y'
+
+while continuar == 'y':
+    lado = random.randint(1,6)
+    print(dado[lado])
+    continuar = input("Quiere tirar otra vez?: y/n ")
+    os.system('cls')
+    if continuar == "n":
+        print("Gracias por jugar, volvé cuando quieras!")
+
